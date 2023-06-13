5c7ff108a1f254befeb849a672c37040d6807d7e
Camiletb <caena@alumni.uv.es>
2023-06-13 14:06:32 +0200

    extend model
    In the future we can improve the relations and add some entity as the status, that can be worked as Type and Language

--- V2/pj_tracoll/app_tracoll/admin.py
+++ V2/pj_tracoll/app_tracoll/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Author, Text
+from .models import Author, Text, AuthorType, TextType, Language, Translation
 
 admin.site.register(Author)
 admin.site.register(Text)
+admin.site.register(AuthorType)
+admin.site.register(TextType)
+admin.site.register(Language)
+admin.site.register(Translation)
--- V2/pj_tracoll/app_tracoll/models.py
+++ V2/pj_tracoll/app_tracoll/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class TextType(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter the text typology: poem, song..." )
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+class AuthorType(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter the author type: poet, singer, band..." )
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter the language")
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
 class Author(models.Model):
 
     POET = 'PO'
@@ -13,7 +40,9 @@ class Author(models.Model):
         (BAND, 'Band'),
     ]
 
-    type = models.CharField(max_length=2, choices=type_choices, default=SINGER)
+    #type = models.CharField(max_length=2, choices=type_choices, default=SINGER)
+    type = models.ForeignKey(AuthorType, on_delete=models.SET_NULL, null=True, blank=True, help_text="Enter the type of author")
+
     name = models.CharField(max_length=100)
     
     class Meta:
@@ -56,11 +85,13 @@ class Text(models.Model):
         (TOTALLY_TRANSLATED, ' (V) Translated'),
     ]
 
-    type = models.CharField(max_length=2, choices=type_choices, default=SONG)
-    language = models.CharField(max_length=2, choices=language_choices)
-    title = models.CharField(max_length=100)
-    content = models.TextField(max_length=2000)
-    state = models.CharField(max_length=1, choices=state_choices, default=NOT_TRANSLATED)
+    #type = models.CharField(max_length=2, choices=type_choices, default=SONG)
+    #language = models.CharField(max_length=2, choices=language_choices)
+    type = models.ForeignKey(TextType, on_delete=models.SET_NULL, null=True, blank=True, help_text="Enter the type of text")
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True, blank=True, help_text="Enter the original language")
+    title = models.CharField(max_length=100, help_text="Enter the title [Max. 100 letters]")
+    content = models.TextField(max_length=2000, help_text="Enter the text you want translate [Max. 2000 letters]")
+    state = models.CharField(max_length=1, choices=state_choices, default=NOT_TRANSLATED, help_text="Enter the translation state")
 
     #author = models.ForeignKey(Author, blank=True, null = True, on_delete = models.SET_NULL)
     authors = models.ManyToManyField(Author, blank=True)
@@ -71,3 +102,23 @@ class Text(models.Model):
         print_authors = ", ".join([author.name for author in self.authors.all()])
         return f'{self.title}, {print_authors} [{self.state}]'
         #return f'{self.title}, {self.author} [{self.state}]'
+
+import uuid  # Required for unique book instances
+from datetime import date
+from django.contrib.auth.models import User  # Required to assign User as a borrower
+
+class Translation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this translation")
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=2000)
+    original_text = models.ForeignKey(Text, on_delete=models.RESTRICT, null=True)
+    user_translator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        ordering = ['title', 'original_text']
+
+    def __str__(self):
+        print_authors = ", ".join([author.name for author in self.original_text.authors.all()])
+        #return f'{self.id}: {self.title}({self.original_text.title}, {print_authors})'
+        return f'{self.title}, {print_authors} [from {self.original_text.language}]'
+         
